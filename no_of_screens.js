3b784b9d8f1338c6c29453b4752649f710acb08d
Satyajeet Ghosh <103898124+SatyajeetGhosh@users.noreply.github.com>
2023-03-06 14:18:48 +0530

    Add files via upload

---
+++ no_of_screens.js
@@ -0,0 +1,21 @@
+// const electron = require('electron')
+// const { app } = electron
+
+// app.on('ready', () => {
+//   const screens = electron.screen.getAllDisplays()
+
+//   if(screens.length > 1){
+//     // TODO: Do something when there's more than one display
+//     alert("More than one screen detected");
+//   }
+// })
+
+// var screens = window.screen;
+// var numberOfScreens = screens.length;
+// for (var i = 0; i < numberOfScreens; i++) {
+//   console.log(`Screen ${i+1} resolution: ${screens[i].width}x${screens[i].height}`);
+// }
+// console.log(`The user has ${numberOfScreens} screen(s) connected.`);
+
+
+
